2626c1b8fc89148b5559a15d3a0ac99264d55e6e
Ede Harrison <ede_harrison@edes-mbp.home>
2023-02-22 11:12:50 +0000

    6

--- controllers/error-handling-controller.js
+++ controllers/error-handling-controller.js
@@ -1,4 +1,17 @@
+exports.customErrors = (err, req, res, next) => {
+    if(err === 'no article here') {
+        res.status(404).send({ message: "Path not found" });
+    } else {
+        next(err)
+    }
+}
+
+exports.PSQLErrors = (err, req, res, next) => {
+    if(err.code === '22P02') {
+        res.status(400).send({ message: 'Bad request' })
+    }
+}
+
 exports.error500 = (err, req, res, next) => {
     res.status(500).send({ message: 'Internal Server Error :(' })
 }
-
